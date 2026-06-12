6673563d9d4de8a1831141402d76a31f026def3b
Manveer Kaur <manveerkaur302006@gmail.com>
2026-06-12 15:24:43 +0530

    first commit

---
+++ frontend/app/hooks/use-workspace.ts
@@ -0,0 +1,72 @@
+import type { WorkspaceForm } from "@/components/workspace/create-workspace";
+import { fetchData, postData } from "@/lib/fetch-util";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+export const useCreateWorkspace = () => {
+  return useMutation({
+    mutationFn: async (data: WorkspaceForm) => postData("/workspaces", data),
+  });
+};
+
+export const useGetWorkspacesQuery = () => {
+  return useQuery({
+    queryKey: ["workspaces"],
+    queryFn: async () => fetchData("/workspaces"),
+  });
+};
+
+export const useGetWorkspaceQuery = (
+  workspaceId?: string | null
+) => {
+  return useQuery({
+    queryKey: ["workspace", workspaceId],
+    enabled: !!workspaceId,
+    queryFn: async () =>
+      fetchData(`/workspaces/${workspaceId}/projects`),
+  });
+};
+
+export const useGetWorkspaceStatsQuery = (
+  workspaceId?: string | null
+) => {
+  return useQuery({
+    queryKey: ["workspace", workspaceId, "stats"],
+    enabled: !!workspaceId,
+    queryFn: async () =>
+      fetchData(`/workspaces/${workspaceId}/stats`),
+  });
+};
+
+export const useGetWorkspaceDetailsQuery = (
+  workspaceId?: string | null
+) => {
+  return useQuery({
+    queryKey: ["workspace", workspaceId, "details"],
+    enabled: !!workspaceId,
+    queryFn: async () =>
+      fetchData(`/workspaces/${workspaceId}`),
+  });
+};
+
+export const useInviteMemberMutation = () => {
+  return useMutation({
+    mutationFn: (data: { email: string; role: string; workspaceId: string }) =>
+      postData(`/workspaces/${data.workspaceId}/invite-member`, data),
+  });
+};
+
+export const useAcceptInviteByTokenMutation = () => {
+  return useMutation({
+    mutationFn: (token: string) =>
+      postData(`/workspaces/accept-invite-token`, {
+        token,
+      }),
+  });
+};
+
+export const useAcceptGenerateInviteMutation = () => {
+  return useMutation({
+    mutationFn: (workspaceId: string) =>
+      postData(`/workspaces/${workspaceId}/accept-generate-invite`, {}),
+  });
+};
